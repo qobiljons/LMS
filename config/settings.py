5f5c8600ccbd2aae85f80a57323485f807a40347
Qobiljon Xayrullayev <qobiljonkhayrullayev@gmail.com>
2024-07-12 20:38:35 +0500

    adding database url

--- config/settings.py
+++ config/settings.py
@@ -85,16 +85,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.getenv('DB_HOST'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'NAME': os.getenv('DB_NAME'),
-        'PORT': os.getenv('DB_PORT', '5432'),  # Default PostgreSQL port
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 # Password validation
