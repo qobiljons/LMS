d9a561a7569cc7039676e3f4456487ad2b0132a1
Qobiljon Xayrullayev <qobiljonkhayrullayev@gmail.com>
2024-07-12 11:20:42 +0500

    defining allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
