7926619f0f72c569713a6f26b85f3e8366804e76
Qobiljon Xayrullayev <qobiljonkhayrullayev@gmail.com>
2024-07-12 20:24:06 +0500

    ,

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     # 'core.apps.CoreConfig',
-    'core'
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
